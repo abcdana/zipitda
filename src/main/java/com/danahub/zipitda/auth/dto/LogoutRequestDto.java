6aeff6b8dfc6e1adb0b3aedc8fa289179c984742
Daeun Kim <74163296+abcdana@users.noreply.github.com>
2025-02-26 20:35:56 +0900

    fix: 로그아웃 API 수정 @RequestParam에서 @RequestBody로 변경

---
+++ src/main/java/com/danahub/zipitda/auth/dto/LogoutRequestDto.java
@@ -0,0 +1,12 @@
+package com.danahub.zipitda.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LogoutRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+}
