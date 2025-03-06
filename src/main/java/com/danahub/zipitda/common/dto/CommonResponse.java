15a16c33564a56ab3f496b4fe84c14c67bd06a42
Daeun Kim <74163296+abcdana@users.noreply.github.com>
2025-03-06 16:01:44 +0900

    refactor: CommonResponse success() 오버로딩 추가

--- src/main/java/com/danahub/zipitda/common/dto/CommonResponse.java
+++ src/main/java/com/danahub/zipitda/common/dto/CommonResponse.java
@@ -6,6 +6,10 @@ public record CommonResponse<T>(int code, String message, T data) {
         return new CommonResponse<>(0, "Success", data);
     }
 
+    public static <T> CommonResponse<T> success() {
+        return new CommonResponse<>(0, "Success", null);
+    }
+
     public static <T> CommonResponse<T> error(int code, String message) {
         return new CommonResponse<>(code, message, null);
     }
