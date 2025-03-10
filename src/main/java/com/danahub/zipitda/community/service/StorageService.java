50139a31d74c116aefc90655cefb69ee8a53db86
Daeun Kim <74163296+abcdana@users.noreply.github.com>
2025-03-10 10:18:47 +0900

    fix: 예외처리 코드리뷰사항 수정 - logConsumer에 log::warn 담기

--- src/main/java/com/danahub/zipitda/community/service/StorageService.java
+++ src/main/java/com/danahub/zipitda/community/service/StorageService.java
@@ -3,6 +3,7 @@ package com.danahub.zipitda.community.service;
 import com.danahub.zipitda.common.exception.ErrorType;
 import com.danahub.zipitda.common.exception.ZipitdaException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StorageService {
 
     private static final String UPLOAD_DIR = "src/main/resources/static/uploads/";
@@ -28,8 +30,8 @@ public class StorageService {
             file.transferTo(destinationFile);
         } catch (Exception e) {
             throw new ZipitdaException(ErrorType.INTERNAL_SERVER_ERROR,
-                                        Map.of("fileName", fileName, "originalFileName", file.getOriginalFilename()),
-                                        log -> System.err.println(log),
+                                        Map.of("fileName", fileName, "originalFileName", file.getOriginalFilename())
+                                        ,log::warn,
                                         e);
         }
 
@@ -42,21 +44,21 @@ public class StorageService {
         File file = new File(filePath);
 
         if (file.exists() && file.delete()) {
-            System.out.println("파일 삭제 완료: " + filePath);
+            log.info("파일 삭제 완료: " + filePath);
         } else {
-            throw new ZipitdaException(ErrorType.RESOURCE_NOT_FOUND, Map.of("message", "삭제할 파일이 존재하지 않습니다."));
+            throw new ZipitdaException(ErrorType.RESOURCE_NOT_FOUND, Map.of("filePath", filePath));
         }
     }
 
     // 파일 검증 (확장자 체크)
     private void validateFile(MultipartFile file) {
         if (file.isEmpty()) {
-            throw new ZipitdaException(ErrorType.INVALID_REQUEST, Map.of("message", "업로드할 파일이 없습니다."));
+            throw new ZipitdaException(ErrorType.INVALID_REQUEST);
         }
 
         String fileName = file.getOriginalFilename();
         if (fileName == null || !fileName.matches(".*\\.(png|jpg|jpeg|gif|bmp)$")) {
-            throw new ZipitdaException(ErrorType.UNSUPPORTED_REQUEST_TYPE, Map.of("message", "지원하지 않는 파일 형식입니다."));
+            throw new ZipitdaException(ErrorType.UNSUPPORTED_REQUEST_TYPE, Map.of("fileName", fileName));
         }
     }
 }
